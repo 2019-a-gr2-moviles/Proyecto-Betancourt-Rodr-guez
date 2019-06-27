77299371328a00f88b751d6ac549361fc766f543
Cristian Betancourt <cristianbb98@gmail.com>
2019-06-27 10:18:33 -0500

    Modificacion Persona.js

--- Backend/foodtruck/api/models/Persona.js
+++ Backend/foodtruck/api/models/Persona.js
@@ -9,21 +9,30 @@ module.exports = {
 
   attributes: {
 
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+    nombre_prs: {
+      type: 'string',
+      minLength: 3,
+      maxLength: 15,
+      required: true,
+    },
+    apellido_prs: {
+      type: 'string',
+      minLength: 4,
+      maxLength: 15,
+      required: true,
+    },
+    cedula: {
+      type: 'string',
+      required: true,
+      unique: true,
+      minLength: 10,
+      maxLength: 25,
+    },
+    fecha_nacimiento: {
+      type: 'string',
+      maxLength: 10,
+      required: true
+    }
   },
-
 };
 
