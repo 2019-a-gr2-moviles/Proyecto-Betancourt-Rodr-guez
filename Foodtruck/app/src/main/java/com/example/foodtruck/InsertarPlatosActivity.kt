8539582830446ea6c3245a8e6cc5bfc669d63b4d
marcorodriguez11 <mar_rs_123@hotmail.com>
2019-06-28 17:50:19 -0500

    creacion de actividades

---
+++ Foodtruck/app/src/main/java/com/example/foodtruck/InsertarPlatosActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodtruck
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InsertarPlatosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insertar_platos)
+    }
+}
